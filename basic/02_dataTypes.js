5e0a1c439646eeb6f84511b7a21cba5f07421904
Sagar Dhawlapure <sdhawalapure@gmail.com>
2024-12-30 15:18:14 +0000

    variable

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code as a newer version.
+
+//alert(3+3) we are using node.js, not browser
+
+let name="sagar"
+let age=18
+let isLoggedIn=false;
+
+console.log(typeof isLoggedIn)
---
+++ basic/variables.js
@@ -0,0 +1,14 @@
+const accountId=14453//we can not change the value
+let accountEmail="sagarDhawlapure@gmail.com"
+var accountPassword="12345"
+accountCity="Nanded"
+let accountState;
+
+//accountId=2
+console.log(accountId);
+
+accountEmail="sagar@gmail.com"
+console.log(accountEmail);
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
